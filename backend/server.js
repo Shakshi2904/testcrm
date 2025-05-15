9029a02488e33892fe91a98b9574155b4eb1b782
Shakshi2904 <132297826+Shakshi2904@users.noreply.github.com>
2025-05-15 15:00:19 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -56,6 +56,6 @@ app.get("/*", function (req, res) {
 });
 
 // Start server
-app.listen(5000, "0.0.0.0", () => {
-  console.log("Server running on port 5000");
+app.listen(80, "0.0.0.0", () => {
+  console.log("Server running on port 80");
 });
